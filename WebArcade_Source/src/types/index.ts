8f516885e7be0b15e8926da4d8f8c6e4f1dd72c8
Alis4211 <136129198+Alis4211@users.noreply.github.com>
2026-09-16 18:41:44 +0500

    Delete WebArcade_Source directory

--- WebArcade_Source/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export type GameState = 'menu' | 'playing' | 'gameover';
-
-export interface BaseGameProps {
-  onScoreUpdate: (score: number) => void;
-  onGameOver: () => void;
-  isPaused?: boolean;
-}
